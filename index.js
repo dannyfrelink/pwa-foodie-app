61ede3f581572201ce56afd7cfbb7bf006a78a58
dannyfrelink <danny-frelink@hotmail.com>
2022-03-21 12:17:08 +0100

    first res.send(hello world)

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require(`express`);
+const app = express();
+const port = 5555;
+
+app.use(express.static(`static/public`));
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use((req, res) => {
+    res.status(404).send(`Sorry, deze pagina kon ik niet vinden.`);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
