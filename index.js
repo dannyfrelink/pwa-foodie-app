ef3771e3b33acce4b6390006832ac15ee7ad27f8
dannyfrelink <danny-frelink@hotmail.com>
2022-03-21 12:46:49 +0100

    add head & footer.ejs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const app = express();
 const port = 5555;
 
-app.use(express.static(`static/public`));
+app.use(express.static(`public`));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
