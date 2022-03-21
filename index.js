53121e98b425b8dcd6451c9e9e6ffd0881edc19d
dannyfrelink <danny-frelink@hotmail.com>
2022-03-21 12:59:18 +0100

    add home page with ejs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require(`express`);
+const express = require('express');
 const app = express();
 const port = 5555;
 
-app.use(express.static(`public`));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
+});
+
+app.get('/barcode', async (req, res) => {
+    res.render('barcode');
 });
 
 app.use((req, res) => {
-    res.status(404).send(`Sorry, deze pagina kon ik niet vinden.`);
+    res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
 });
 
 app.listen(port, () => {
