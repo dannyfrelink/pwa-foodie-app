d1b5e52288e251e8746490cef3cc11c4119b438e
dannyfrelink <danny-frelink@hotmail.com>
2022-03-28 11:53:29 +0200

    trying to implement build process

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 5555;
 
-app.use(express.static('public'));
+app.use(express.static('src'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
