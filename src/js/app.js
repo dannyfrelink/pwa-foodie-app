fbbdfc5f729eba2e73e161f4ca3816f0db4505d2
dannyfrelink <danny-frelink@hotmail.com>
2022-03-29 13:35:40 +0200

    get offline state

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('../service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then(function (registration) {
                 return registration.update();
             })
