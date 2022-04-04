f4c6b88a2d3beb2c40f2f9751a4da7d0c3f55963
dannyfrelink <danny-frelink@hotmail.com>
2022-04-04 15:44:06 +0200

    remove unused code

--- src/js/modules/showSection.js
+++ src/js/modules/showSection.js
@@ -1,17 +1,8 @@
 import { startLoader } from './loader.js';
-import { html, body, barcodeSection, videoDiv } from './variables.js';
+import { html, body } from './variables.js';
 
-const showBarcodeSection = () => {
+export const showBarcodeSection = () => {
     html.classList.add('overflow_hidden');
     body.classList.add('overflow_hidden');
     startLoader();
 }
-
-const showProductSection = () => {
-    html.classList.add('overflow_hidden');
-    body.classList.remove('overflow_hidden');
-    videoDiv.innerHTML = '';
-    barcodeSection.classList.remove('infaden');
-}
-
-export { showBarcodeSection, showProductSection }
--- src/js/modules/stopScanner.js
+++ src/js/modules/stopScanner.js
@@ -3,9 +3,7 @@ import { body, html, video, videoDiv } from "./variables.js";
 export default function stopDetecting() {
     html.classList.remove('overflow_hidden');
     body.classList.remove('overflow_hidden');
-    // barcodeSection.classList.remove('infaden');
     videoDiv.innerHTML = '';
-    // loader.classList.add('hidden');
     stopScanner();
 }
 
