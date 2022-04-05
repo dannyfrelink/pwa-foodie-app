94bf57c7a42b53bfa3bf789d22326454b92063d8
dannyfrelink <danny-frelink@hotmail.com>
2022-04-05 14:00:41 +0200

    add all client-side js in one file

--- build_scripts/build-js.js
+++ build_scripts/build-js.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 gulp.src([
-    './src/js/*.js'
+    './src/js/modules/variables.js',
+    './src/js/modules/loader.js',
+    './src/js/modules/showSection.js',
+    './src/js/modules/stopScanner.js',
+    './src/js/modules/startScanner.js',
+    './src/js/app.js'
 ])
     .pipe(concat('index.js'))
     .pipe(gulp.dest('./static/'))
-
-gulp.src([
-    './src/js/modules/*.js'
-])
-    .pipe(gulp.dest('./static/modules'))
