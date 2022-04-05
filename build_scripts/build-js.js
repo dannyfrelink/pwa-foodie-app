5d872aa7f7cc2a857d2a6b561c95a9c5d3121642
dannyfrelink <danny-frelink@hotmail.com>
2022-04-05 14:15:38 +0200

    add uglify to js file

--- build_scripts/build-js.js
+++ build_scripts/build-js.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.src([
     './src/js/modules/variables.js',
@@ -9,5 +10,6 @@ gulp.src([
     './src/js/modules/startScanner.js',
     './src/js/app.js'
 ])
+    .pipe(uglify())
     .pipe(concat('index.js'))
     .pipe(gulp.dest('./static/'))
